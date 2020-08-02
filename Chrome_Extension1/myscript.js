f8bdeccca7ccb175ddb24a8668b4abd23bc17fe7
Gargee-srivastava <39323310+Gargee-srivastava@users.noreply.github.com>
2020-08-02 23:07:23 +0530

    Update myscript.js

--- Chrome_Extension1/myscript.js
+++ Chrome_Extension1/myscript.js
@@ -2,7 +2,7 @@
  *DISCLAIMER: Any Part or segment of the comments presented as documentation cannot be modified or removed without the proper permission from the current author. If any of the documentation in the public domain is found without proper credits and permission of the author, it will be dealt as plagiarism of the original code. However, part of the codes can be customized and used as per needs without any permission for personal use.
  *Author: GARGEE SRIVASTAVA
  *Contact details: srivastava.gargee@gmail.com
- *Developed for: Affective Computing Team, IIT-Guwahati for development of vedinkakSa, a sensitive classroom application.
+ *Developed for:Smart India Hackathon 2020
  */
 var url=chrome.extension.getURL('toolbar.html');
 var height="10px";
