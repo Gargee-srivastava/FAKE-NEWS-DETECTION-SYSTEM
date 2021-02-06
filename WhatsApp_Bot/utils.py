fb80d566ce347230bbeb826314e965481b06eaa4
Gargee-srivastava <39323310+Gargee-srivastava@users.noreply.github.com>
2021-02-06 20:21:43 +0530

    Update utils.py

--- WhatsApp_Bot/utils.py
+++ WhatsApp_Bot/utils.py
@@ -1,3 +1,10 @@
+
+ ##DISCLAIMER: Any Part or segment of the comments presented as documentation cannot be modified or removed without the proper permission from the current author. If any of the documentation in the public domain is found without proper credits and permission of the author, it will be dealt as plagiarism of the original code. However, part of the codes can be customized and used as per needs without any permission for personal use.
+ #Author: GARGEE SRIVASTAVA
+ #Contact details: srivastava.gargee@gmail.com
+ #Developed for: Smart India Hackathon 2020
+ 
+
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="jokes-jjcedq-ba06145b949e.json"
 import dialogflow_v2 as dialogflow
